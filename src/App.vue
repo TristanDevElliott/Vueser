6bcab6d5524a1d2198a48d0155889950a931abce
Tristan Elliott <tristan@tristanelliott.co.za>
2020-06-30 15:38:13 -0700

    Editing the Api Server

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,29 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="min-h-screen bg-gray-100">
+      <div class="min-h-screen flex">
+        <div class="w-2/12 bg-gray-700">
+          <img
+            src="./assets/logo.png"
+            alt="Vueser"
+            class="flex items-center img-brand"
+          />
+          <router-link to="/" class="block text-gray-200 mt-5 p-5"
+            >Home</router-link
+          >
+          <router-link to="/about">About</router-link>
+        </div>
+        <div class="flex-1">
+          <router-view />
+        </div>
+      </div>
     </div>
-    <router-view />
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.img-brand {
+  width: 50px;
   text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
